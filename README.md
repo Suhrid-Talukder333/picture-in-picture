a0c7e00c248bcd7b14f4198b032ff951f99fcbb5
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-05-24 11:25:54 +0600

    picture in picture implemented in the button

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# picture-in-picture
+<h1>Picture in Picture</h1>
+<div>
+  <img src="" />
+</div>
+</br></br>
+<p>This is a fun project and I use it to watch videos from youtube while surfing the web for answers :)</p>
+
+## Bugs
+
+It seems that the media screen share in firefox doesn't show other windows in screen share prompt. To use this for your own
+use chrome or brave browser.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 const videoElement = document.getElementById("video");
+const buttonElement = document.getElementById("button");
+
+//select media screen and pass it to video element
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
+
+selectMediaStream();
+
+buttonElement.addEventListener("click", async () => {
+  buttonElement.disabled = true;
+  await videoElement.requestPictureInPicture();
+  buttonElement.disabled = false;
+});
